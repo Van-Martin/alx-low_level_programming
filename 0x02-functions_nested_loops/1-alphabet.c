79b8d0f79a26de6352548552d485ed07d93ce9d7
Van-Martin <martindotsey526@gmail.com>
2022-07-06 16:37:12 -0700

    "Nested loops"

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void)
+void print_alphabet(void);
 {
-	int i;
+	int i = 97;
 
-	for (i = 97; i <= 122; i++)
+	for ( i <= 122; )
+		i++;
 	{
 		_putchar(i);
 	}
