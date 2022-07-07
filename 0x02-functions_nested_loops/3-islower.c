b32f99af5b1cd87d48b3e7bfcd657f330967408d
Van-Martin <martindotsey526@gmail.com>
2022-07-06 18:58:18 -0700

    "Good"

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
-#include "main"
+#include "main.h"
 /**
  * -islower - Shows 1 if the input ia s lower case character. Another cases shows 0
  *
  *  Return: 1 for lowercase. 0 for others.
  */
-int islower(int c)
+int _islower(int c)
 
 {
 	char c;
