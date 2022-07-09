dd4e5c3611b9a5d09036b9424d8712d224016ce2
Van-Martin <106705044+Van-Martin@users.noreply.github.com>
2022-07-09 01:51:27 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,7 @@
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0 (Success)
- */
+#ifndef _holberton_h_
+#define _holberton_h_
 
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +9,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-(
-
-
+#endif
